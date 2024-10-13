38757cf9d6b1d57fd7d3973f8619996afd3eb6dd
Adrian <adrian.kok@mail.utoronto.ca>
2024-10-13 19:01:03 +0800

    added google analytics to be optional

--- src/main.js
+++ src/main.js
@@ -17,15 +17,18 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(
-  VueGtag,
-  {
-    pageTrackerScreenviewEnabled: true,
-    config: { id: import.meta.env.VITE_GOOGLE_ANALYTICS_ID },
-    appName: 'adrian-blog'
-  },
-  router
-)
+
+if (import.meta.env.VITE_GOOGLE_ANALYTICS_ID) {
+  app.use(
+    VueGtag,
+    {
+      pageTrackerScreenviewEnabled: true,
+      config: { id: import.meta.env.VITE_GOOGLE_ANALYTICS_ID },
+      appName: 'adrian-blog'
+    },
+    router
+  )
+}
 
 app.component('page-image', PageImage)
 
