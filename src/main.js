68392a88880262a9fbb6d033ea6b46183ac5f794
Adrian <adrian.kok@mail.utoronto.ca>
2024-10-17 15:17:05 +0800

    gtag update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router/router'
-import VueGtag from 'vue-gtag'
+// import VueGtag from 'vue-gtag'
 
 import PageImage from './components/ui/PageImage.vue'
 import BaseContent from './components/ui/BaseContent.vue'
@@ -18,17 +18,17 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
-if (import.meta.env.VITE_GOOGLE_ANALYTICS_ID) {
-  app.use(
-    VueGtag,
-    {
-      pageTrackerScreenviewEnabled: true,
-      config: { id: import.meta.env.VITE_GOOGLE_ANALYTICS_ID },
-      appName: 'adrian-blog'
-    },
-    router
-  )
-}
+// if (import.meta.env.VITE_GOOGLE_ANALYTICS_ID) {
+//   app.use(
+//     VueGtag,
+//     {
+//       pageTrackerScreenviewEnabled: true,
+//       config: { id: import.meta.env.VITE_GOOGLE_ANALYTICS_ID },
+//       appName: 'adrian-blog'
+//     },
+//     router
+//   )
+// }
 
 app.component('page-image', PageImage)
 
